93ba7d5ae6eeaf19ecec3660ae5505812719b23e
lvjing <lvjing@finupgroup.com>
2020-06-23 16:22:46 +0800

    [add]增加学习笔记

---
+++ src/test/java/testframwork/UiAuto.java
@@ -0,0 +1,14 @@
+package testframwork;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @author jingLv
+ * @date 2020-06-18 6:16 下午
+ */
+public class UiAuto {
+    public String name = "";
+    public String description = "";
+    public List<HashMap<String, Object>> steps;
+}
